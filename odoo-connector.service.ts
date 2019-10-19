0341eae2a78b0cdc960087912463250dc7aabb03
Ignacio Buioli <ibuioli@gmail.com>
2019-10-19 15:39:36 -0300

    feat: add renderReport fun

--- odoo-connector.service.ts
+++ odoo-connector.service.ts
@@ -147,4 +147,29 @@ export class OdooConnector {
 
     return odoo$;
   }
+
+  public renderReport(model: string, id: number): any {
+    console.log('Render Report on:', model);
+    const odoo$ = new Observable(observer => {
+      $.xmlrpc({
+        url: this.server + '/xmlrpc/2/report',
+        methodName: 'render_report',
+        dataType: 'xmlrpc',
+        crossDomain: true,
+        params: [this.db, this.uid, this.pass, model, [id]],
+        success: (response: any, status: any, jqXHR: any) => {
+          console.log('Render Report, ' + model + ' status:', status);
+          observer.next(response);
+          observer.complete();
+        },
+        error: (jqXHR: any, status: any, error: any) => {
+          console.log('Render Report, ' + model + ' status:', status);
+          console.log('Err:', error);
+          observer.error(error);
+        }
+      });
+    });
+
+    return odoo$;
+  }
 }
